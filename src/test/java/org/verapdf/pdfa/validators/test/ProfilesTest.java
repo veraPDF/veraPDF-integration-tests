e34b8bf78c35e4fe701d43412de6578383b47300
MaximPlusov <maxplusim@mail.ru>
2021-02-17 16:32:38 +0300

    REL - veraPDF v1.18 - bumped minor version to 1.18 - using parent POM v1.18.1 - updated Maven dependencies

--- src/test/java/org/verapdf/pdfa/validators/test/ProfilesTest.java
+++ src/test/java/org/verapdf/pdfa/validators/test/ProfilesTest.java
@@ -36,7 +36,7 @@ import org.verapdf.pdfa.validation.profiles.ValidationProfile;
  */
 @SuppressWarnings("static-method")
 public class ProfilesTest {
-    private static final org.verapdf.pdfa.validation.profiles.ProfileDirectory INTEGRATION_PROFILES = GitHubBackedProfileDirectory.fromBranch("pdfua-prerelease");
+    private static final org.verapdf.pdfa.validation.profiles.ProfileDirectory INTEGRATION_PROFILES = GitHubBackedProfileDirectory.fromBranch("rel/1.18");
 
     /**
      * Test method for {@link org.verapdf.pdfa.validation.ValidationProfileImpl#getPDFAFlavour()}.

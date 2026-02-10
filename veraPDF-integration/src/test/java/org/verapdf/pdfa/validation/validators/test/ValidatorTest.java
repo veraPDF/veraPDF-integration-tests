e662f176dba59d03c2ee4cad5ccbdcbb34c1e674
Maxim <maxplusim@mail.ru>
2026-02-10 20:23:30 +0300

    RC - v1.30 (jakarta)

--- veraPDF-integration/src/test/java/org/verapdf/pdfa/validation/validators/test/ValidatorTest.java
+++ veraPDF-integration/src/test/java/org/verapdf/pdfa/validation/validators/test/ValidatorTest.java
@@ -62,7 +62,7 @@ import static org.junit.Assert.*;
  */
 @SuppressWarnings("static-method")
 public class ValidatorTest {
-    private static final ProfileDirectory PROFILES = GitHubBackedProfileDirectory.fromBranch("integration");
+    private static final ProfileDirectory PROFILES = GitHubBackedProfileDirectory.fromBranch("rc/1.30");
 
     @BeforeClass
     public static final void SetUp() throws IOException {
